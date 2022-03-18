b0cffc9dc1644bbd35bfe34ebbe199a91caec9ea
zjw981119 <962610351@qq.com>
2022-03-18 15:56:08 -0400

    Add tuit stats in Tuit model, implement; userTogglesTuitLikes in LikeController

--- models/tuits/Tuit.ts
+++ models/tuits/Tuit.ts
@@ -1,13 +1,16 @@
 import User from "../users/User";
+import Stats from "./Stats";
 
 /**
  * @class Tuit Represents a tuit posted by a user
  * @property {string} tuit tuit's content
  * @property {Date} postedOn tuit's creation time
  * @property {ObjectId} postedBy user reference
+ * @property {Stats} stats stats of tuit
  */
 export default class Tuit{
-    private tuit: string = '';
-    private postedOn: Date = new Date();
-    private postedBy: User | null = null;
+    tuit: string = '';
+    postedOn: Date = new Date();
+    postedBy: User | null = null;
+    stats: Stats
 }
