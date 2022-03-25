914aa519c9ddb96f7c0293cb9ee84960d69ff510
zjw981119 <962610351@qq.com>
2022-03-25 15:25:06 -0400

    pass express app to dislikeController

--- server.ts
+++ server.ts
@@ -22,6 +22,7 @@ import FollowController from "./controllers/FollowController";
 import BookmarkController from "./controllers/BookmarkController";
 import MessageController from "./controllers/MessageController";
 import AuthenticationController from "./controllers/AuthenticationController";
+import DislikeController from "./controllers/DislikeController";
 const cors = require('cors')
 const session = require("express-session");
 
@@ -113,6 +114,7 @@ app.get('/add/:a/:b', (req, res) => {
 const userController = UserController.getInstance(app);
 const tuitController = TuitController.getInstance(app);
 const likeController = LikeController.getInstance(app);
+const dislikeController = DislikeController.getInstance(app)
 const followController = FollowController.getInstance(app);
 const bookmarkController = BookmarkController.getInstance(app);
 const messageController = MessageController.getInstance(app);
